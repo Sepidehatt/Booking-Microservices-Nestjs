499de53826efc389675d9e60eda6b7460cf76b2a
Sepideh Attar <sepideh.att@gmail.com>
2024-02-20 16:56:23 +0100

    Added resevation repo and entity

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/common';
+import { ReservationRepository } from './reservation.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './entities/reservation.entity';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationRepository],
 })
 export class ReservationsModule {}
